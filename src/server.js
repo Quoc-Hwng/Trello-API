89bcb5cf08c7c002cfbac332b9ae8f3ab39ff853
Quoc-Hwng <phamhung151020@gmail.com>
2025-02-14 16:00:34 +0700

    Express Router

--- src/server.js
+++ src/server.js
@@ -2,14 +2,13 @@ import express from 'express'
 import { CLOSE_DB, CONNECT_DB } from './config/mongodb'
 import exitHook from 'async-exit-hook'
 import { env } from '~/config/environment'
+import { APIs_V1 } from './routes/v1'
 
 const START_SERVER = () => {
 
     const app = express()
 
-    app.get('/', function (req, res) {
-        res.send('Hehe')
-    })
+    app.use('/v1', APIs_V1)
 
     app.listen(env.APP_PORT, env.APP_HOST, () => {
         console.log(`Server running at http://${env.APP_HOST}:${env.APP_PORT}`)
