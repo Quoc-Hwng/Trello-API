e4d7e2670284b89148ff81f7b3651ce9d08739f9
Quoc-Hwng <phamhung151020@gmail.com>
2025-02-15 15:33:59 +0700

    Cors

--- src/server.js
+++ src/server.js
@@ -1,13 +1,17 @@
 import express from 'express'
+import cors from 'cors'
 import { CLOSE_DB, CONNECT_DB } from './config/mongodb'
 import exitHook from 'async-exit-hook'
 import { env } from '~/config/environment'
 import { APIs_V1 } from './routes/v1'
 import { errorHandlingMiddleware } from './middlewares/errorHandlingMiddleware'
+import { corsOptions } from './config/cors'
 
 const START_SERVER = () => {
 
     const app = express()
+
+    app.use(cors(corsOptions))
 
     //Enable req.body json data
     app.use(express.json())
